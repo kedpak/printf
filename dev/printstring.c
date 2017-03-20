8eb765137bf2220bac1bb5fe7a7c25391a0f161d
kedpak <kedpak@yahoo.com>
2017-03-20 22:46:02 +0000

    print char

---
+++ dev/printstring.c
@@ -0,0 +1,85 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+void p_char(va_list ap)
+{
+	_putchar(va_arg(ap, int));
+}
+        
+unsigned int _printf(const char *format, ...)
+{
+	form_t print_type[] = {
+	      {'c', p_char},
+	      {'\0', NULL}
+	      };
+        unsigned int i = 0;
+	int j = 0;
+/*	static char buffer[1024]; */
+        va_list ap; 
+
+        va_start(ap, format);
+
+
+        for (; format && format[i] != '\0'; i++)
+        {
+		
+		if (format[i] == '%')
+		{
+
+			i++;
+			while (print_type[j].type != '\0')
+			{
+				if (print_type[j].type == format[i])
+				{
+					print_type[j].func(ap);
+				}
+				j++;
+			}
+
+		}
+		else if (format[i] != '%')
+		{
+			_putchar(format[i]);
+		}
+	
+		
+
+
+	}
+
+/*	i = 0;
+        while(buffer[i] != '\0')
+        {
+                _putchar(buffer[i]);
+                i++;
+        }
+        buffer[i] = '\0';
+
+	buffer[i] = '\0'; */
+	va_end(ap);
+	return (i);
+}
+
+int main() 
+{
+	char s;
+
+	s = _printf("hi %c woopie \n", 'm'); 
+	printf("%d is the length of buffer\n", s);
+	return 0;
+}
