3d42bb6a593da064aab844275394b1a8e0b7033f
spacexengineer <hunter.casbeer@gmail.com>
2017-03-21 09:10:21 -0700

    added condition for percent symbols

--- _printf.c
+++ _printf.c
@@ -1,7 +1,6 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include "holberton.h"
-#include <unistd.h>
 /**
  * _printf - Behold, the magic function that should print many things to stdout
  * @format: character pointer checking each character in string given,
@@ -11,41 +10,40 @@
 int _printf(const char *format, ...)
 {
 	form_t print_type[] = {
-		{'c', p_char},
-		{'b', p_binary},
-		{'s', p_string},
-		{'d', p_num},
-		{'i', p_num},
-		{'u', p_unum},
-		{'o', p_octal},
-		{'x', p_hex},
-		{'X', p_hex},
-		{'%', p_percent},
-		{'\0', NULL}
+		{'c', p_char}, {'b', p_binary}, {'s', p_string}, {'d', p_num},
+		{'i', p_num}, {'u', p_unum}, {'o', p_octal}, {'x', p_hex},
+		{'X', p_hex}, {'%', p_percent}, {'\0', NULL}
 	};
 	unsigned int i = 0; int j = 0, characters = 0; va_list ap;
 
 	va_start(ap, format);
-	for (; format && format[i] != '\0'; i++)
+	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
-			i++;
 			j = 0;
 			while (print_type[j].type != '\0')
 			{
-				if (print_type[j].type == format[i])
+				if (print_type[j].type == format[i + 1])
 				{
 					characters += print_type[j].func(ap);
+					i++;
+					break;
 				}
 				j++;
 			}
+			if (print_type[j].type == '\0')
+			{
+				_putchar('%');
+				characters++;
+			}
 		}
-		else if (format[i] != '%')
+		else
 		{
 			_putchar(format[i]);
 			characters++;
 		}
+		i++;
 	}
 	va_end(ap);
 	return (characters);
